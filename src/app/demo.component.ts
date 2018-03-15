3e9f169424e2957c487e6d0787efdf70a43b0e8c
Jago <jago84@gmail.com>
2018-03-15 11:28:57 +0100

    Updated first demo content for map with extra values

--- src/app/demo.component.ts
+++ src/app/demo.component.ts
@@ -7,19 +7,24 @@ import { Component } from '@angular/core';
 })
 export class DemoComponent {
   public mapData = {
-    'US' : { value:  1 },
-    'CA' : { value:  2 },
-    'ES' : { value:  3 },
-    'FR' : { value:  4 },
-    'GB' : { value:  5 },
-    'PT' : { value:  6 },
-    'GR' : { value:  7 },
-    'AU' : { value:  8 },
-    'NZ' : { value:  9 },
-    'JP' : { value:  1 },
-    'MY' : { value:  2 },
-    'CN' : { value:  3 },
-    'SM' : { value:  4 },
-    'UY' : { value:  5 },
+    'CA' : { value:  1, extra: { 'Villeneuve': '', 'Stroll': '' } },
+    'ES' : { value:  2, extra: { 'Alonso': '', 'Sainz': '', 'De la Rosa': '', 'De Villota': '', 'Jordá': '' } },
+    'FR' : { value:  4, extra: { 'Grosjean': '', 'Prost': '' } },
+    'GB' : { value:  17, extra: { 'Hamilton': '', 'Button': '', 'Coulthard': '', 'Mansell': '', 'Hunt': '', 'Wolff': '' } },
+    'PT' : { value:  0, extra: { 'Monteiro': '0' } },
+    'IT' : { value:  3, extra: { 'Trulli': '', 'Lombardi': '', 'Farina': '' } },
+    'MX' : { value:  0, extra: { 'Pérez': '' } },
+    'AT' : { value:  4, extra: { 'Lauda': '' } },
+    'AU' : { value:  4, extra: { 'Ricciardo': '', 'Webber': '', 'Brabham': '' } },
+    'AR' : { value:  5, extra: { 'Fangio': '' } },
+    'RU' : { value:  0, extra: { 'Petrov': '', 'Kvyat': '' } },
+    'US' : { value:  2, extra: { 'Hill': '' } },
+    'FI' : { value:  4, extra: { 'Raikkonen': '', 'Hakkinen': '' } },
+    'JP' : { value:  0, extra: { 'Sato': '', 'Yamamoto': '' } },
+    'NZ' : { value:  1, extra: { 'Hulme': '' } },
+    'ZA' : { value:  1, extra: { 'Scheckter': '' } },
+    'IN' : { value:  0, extra: { 'Karthikeiyan': '', 'Chandhok': '' } },
+    'BR' : { value:  8, extra: { 'Massa': '', 'Barrichello': '', 'Senna': '', 'Piquet': '' } },
+    'DE' : { value:  12, extra: { 'Vettel': '', 'Schumacher': '' } }
   };
 }
