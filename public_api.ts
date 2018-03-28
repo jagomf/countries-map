976200e93a521a36fd354fbf5b3c882a5733b37c
Jago <jago84@gmail.com>
2018-03-28 12:16:37 +0200

    Exposing more data structures

--- public_api.ts
+++ public_api.ts
@@ -1,3 +1,4 @@
 export { CountriesMapModule } from './src/app/modules/countries-map/countries-map.module';
 export { CountriesMapComponent } from './src/app/modules/countries-map/countries-map.component';
-export { ChartErrorEvent, ChartSelectEvent, CountriesData } from './src/app/modules/countries-map/interfaces';
+export { CountriesData, CountryData, CountryExtraData } from './src/app/modules/countries-map/data-types.interface';
+export { ChartErrorEvent, ChartSelectEvent } from './src/app/modules/countries-map/chart-events.interface';
