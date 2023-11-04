ba3a07e04e72064efbef91095280af04ecf01239
Jago (C. Santiago Melón) <santiago.melon@kairosds.com>
2023-11-04 21:12:58 +0100

    Avoids errors while running demo

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,5 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+(window as any).global = window;
