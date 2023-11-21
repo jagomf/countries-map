4218729d063b9f536cb22e841b3eba4235b8e93b
Jago (C. Santiago Melón) <santiago.melon@kairosds.com>
2023-11-21 17:20:57 +0100

    Updated countries list dependency

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { DemoModule } from './app/demo.module';
+import { DemoModule } from './demo/demo.module';
 
 platformBrowserDynamic().bootstrapModule(DemoModule)
   .catch(err => console.error(err));
